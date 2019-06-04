84fed6c20540858221a9a00da188b20dae84dc9d
SatviKaul <satvik.1799@gmail.com>
2019-06-04 11:23:52 +0530

    Create Challenge2.py

---
+++ Challenge2.py
@@ -0,0 +1,21 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables.
+j=2
+f=6.0
+g='is amazing'
+
+# Read and save an integer, double, and String to your variables.
+x=input()
+val=int(x)
+y=input()
+val1=float(y)
+z=input()
+# Print the sum of both integer variables on a new line.
+print(val+i)
+# Print the sum of the double variables on a new line.
+print(d+val1)
+# Concatenate and print the String variables on a new line
+print(s+z)
+# The 's' variable above should be printed first.
