1a258cbb71e8ee4d6bc3cffc7ec4f3e96e8ed47d
SatviKaul <satvik.1799@gmail.com>
2019-06-08 11:18:38 +0530

    Create Day5.py

---
+++ Day5.py
@@ -0,0 +1,15 @@
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input())
+    i=1
+    while(i<=10):
+        a=(n*i)
+        print(n,"x",i,"=",a)
+        i+=1
