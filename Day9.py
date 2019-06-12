de6ed224f44c638f021504d0bee4629b332628ed
SatviKaul <satvik.1799@gmail.com>
2019-06-12 10:48:57 +0530

    Create Day9.py

---
+++ Day9.py
@@ -0,0 +1,15 @@
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the factorial function below.
+def factorial(n):
+    if n<=1:
+        return 1
+    else: 
+        return n*factorial(n-1)
+if __name__=="__main__":
+    print(factorial(int(input())))
+        
